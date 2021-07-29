a189b524ec98e0c5c0ec2f4e440e49b7697a4211
Sina Honarvar <sina.ho79@gmail.com>
2021-07-29 11:10:13 +0430

    some fixes

--- src/mappings/Planter.ts
+++ src/mappings/Planter.ts
@@ -1,7 +1,7 @@
 import {Planter} from "../../generated/schema"
 import {PlanterJoin} from "../../generated/Planter/Planter"
 import { log } from "@graphprotocol/graph-ts";
-export function handlePlanterJoin(event:PlanterJoin){
+export function handlePlanterJoin(event:PlanterJoin): void{
     const planter = new Planter(event.params.planterId.toHex());
     planter.countryCode = "sdf";
     const pl = Planter.load(event.params.planterId.toHex());
