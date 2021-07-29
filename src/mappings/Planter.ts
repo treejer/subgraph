e0ad5461143e8a202c5904ee557adeba19111fb3
Sina Honarvar <sina.ho79@gmail.com>
2021-07-29 10:52:04 +0430

    debug log for planter load

--- src/mappings/Planter.ts
+++ src/mappings/Planter.ts
@@ -1,7 +1,10 @@
 import {Planter} from "../../generated/schema"
 import {PlanterJoin} from "../../generated/Planter/Planter"
+import { log } from "@graphprotocol/graph-ts";
 export function handlePlanterJoin(event:PlanterJoin){
     const planter = new Planter(event.params.planterId.toHex());
     planter.countryCode = "sdf";
+    const pl = Planter.load(event.params.planterId.toHex());
+    log.debug("Planter latitude is {}", [pl.latitude.toString()]);
     planter.save();
 }
