6fc380d4d354359701f2a65ae3f3a6a3ce9ab7a9
Sina Honarvar <sina.ho79@gmail.com>
2021-07-30 02:16:13 +0430

    Required fields fixed

--- src/mappings/Planter.ts
+++ src/mappings/Planter.ts
@@ -1,22 +1,14 @@
 import {Planter} from "../../generated/schema"
 import {PlanterJoin, Planter as PlanterContract} from "../../generated/Planter/Planter"
-import { log } from "@graphprotocol/graph-ts";
+import { BigInt, log } from "@graphprotocol/graph-ts";
 export function handlePlanterJoin(event:PlanterJoin): void{
     let planter = new Planter(event.params.planterId.toHex());
     planter.countryCode = "sdf";
+    planter.status = new BigInt(1);
+    planter.planterType = new BigInt(1);
     let planterContract = PlanterContract.bind(event.address);
     let pl = planterContract.planters(event.params.planterId);
-
-    // let pl = Planter.load(event.params.planterId.toHex());
-    // log.debug("Planter latitude is {}", [pl.latitude.toString()]);
-    log.debug("Planter is {} {} {} {} {} {} {} {} ", [pl.value0.toString(), pl.value1.toString(), pl.value2.toString(), pl.value3.toString(), pl.value4.toString(), pl.value5.toString(), pl.value6.toString(), pl.value7.toString() ]);
+    log.info("Planter is {} {} {} {} {} {} {} {} ", [pl.value0.toString(), pl.value1.toString(), pl.value2.toString(), pl.value3.toString(), pl.value4.toString(), pl.value5.toString(), pl.value6.toString(), pl.value7.toString() ]);
     planter.save();
+    
 }
-
-/**
- *   
-  let contractTreeFactory = TreeFactory.bind(event.address)  
-  theTree.planterRemainingBalance = contractTreeFactory.treeToPlanterRemainingBalance(event.params.treeId)
-  theTree.ambassadorRemainingBalance = contractTreeFactory.treeToAmbassadorRemainingBalance(event.params.treeId)
-  
- */
