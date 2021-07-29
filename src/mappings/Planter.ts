4033337195ebfccb10ef2668aed8543267f425ba
Sina Honarvar <sina.ho79@gmail.com>
2021-07-29 15:41:04 +0430

    some fixes

--- src/mappings/Planter.ts
+++ src/mappings/Planter.ts
@@ -4,7 +4,18 @@ import { log } from "@graphprotocol/graph-ts";
 export function handlePlanterJoin(event:PlanterJoin): void{
     let planter = new Planter(event.params.planterId.toHex());
     planter.countryCode = "sdf";
-    let pl = Planter.load(event.params.planterId.toHex());
+    let planterContract = Planter.bind(event.address);
+    let pl = planterContract.planters(event.params.planterId);
+    
+    // let pl = Planter.load(event.params.planterId.toHex());
     log.debug("Planter latitude is {}", [pl.latitude.toString()]);
     planter.save();
 }
+
+/**
+ *   
+  let contractTreeFactory = TreeFactory.bind(event.address)  
+  theTree.planterRemainingBalance = contractTreeFactory.treeToPlanterRemainingBalance(event.params.treeId)
+  theTree.ambassadorRemainingBalance = contractTreeFactory.treeToAmbassadorRemainingBalance(event.params.treeId)
+  
+ */
