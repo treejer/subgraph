fa3ab29b74528375fdcced4865c890762e5b4fd4
Sina Honarvar <sina.ho79@gmail.com>
2021-07-29 16:40:51 +0430

    some fixes

--- src/mappings/Planter.ts
+++ src/mappings/Planter.ts
@@ -1,14 +1,15 @@
 import {Planter} from "../../generated/schema"
-import {PlanterJoin} from "../../generated/Planter/Planter"
+import {PlanterJoin, Planter as PlanterContract} from "../../generated/Planter/Planter"
 import { log } from "@graphprotocol/graph-ts";
 export function handlePlanterJoin(event:PlanterJoin): void{
     let planter = new Planter(event.params.planterId.toHex());
     planter.countryCode = "sdf";
-    let planterContract = Planter.bind(event.address);
+    let planterContract = PlanterContract.bind(event.address);
     let pl = planterContract.planters(event.params.planterId);
-    
+
     // let pl = Planter.load(event.params.planterId.toHex());
-    log.debug("Planter latitude is {}", [pl.latitude.toString()]);
+    // log.debug("Planter latitude is {}", [pl.latitude.toString()]);
+    log.debug("Planter is {} {} {} {} {} {} {} {} ", [pl.value0.toString(), pl.value1.toString(), pl.value2.toString(), pl.value3.toString(), pl.value4.toString(), pl.value5.toString(), pl.value6.toString(), pl.value7.toString() ]);
     planter.save();
 }
 
