512fb5f59e5bfec6641cc1b8670b4bbfeff7bb98
Sina Honarvar <sina.ho79@gmail.com>
2021-07-29 11:12:19 +0430

    some fixes

--- src/mappings/Planter.ts
+++ src/mappings/Planter.ts
@@ -2,9 +2,9 @@ import {Planter} from "../../generated/schema"
 import {PlanterJoin} from "../../generated/Planter/Planter"
 import { log } from "@graphprotocol/graph-ts";
 export function handlePlanterJoin(event:PlanterJoin): void{
-    const planter = new Planter(event.params.planterId.toHex());
+    let planter = new Planter(event.params.planterId.toHex());
     planter.countryCode = "sdf";
-    const pl = Planter.load(event.params.planterId.toHex());
+    let pl = Planter.load(event.params.planterId.toHex());
     log.debug("Planter latitude is {}", [pl.latitude.toString()]);
     planter.save();
 }
