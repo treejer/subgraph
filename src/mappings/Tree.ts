bab800fb7ebece1639f36cdc071e782d3f72f36a
Amir Habibzadeh <habibzade.amir@gmail.com>
2021-11-24 14:09:55 +0300

    regular and genesis treecount added to Owner

--- src/mappings/Tree.ts
+++ src/mappings/Tree.ts
@@ -7,10 +7,7 @@ import { Tree, Owner } from "../../generated/schema";
 import { Address, BigInt } from '@graphprotocol/graph-ts';
 import { getGlobalData, ZERO_ADDRESS } from '../helpers';
 
-
 export function handleTransfer(event: Transfer): void {
-
-    let treeContract = TreeContract.bind(event.address);
 
     let tree = Tree.load(event.params.tokenId.toHexString());
     if (!tree) {
@@ -21,34 +18,51 @@ export function handleTransfer(event: Transfer): void {
     tree.owner = event.params.to.toHexString();
     tree.save();
 
-    if(event.params.to.notEqual(Address.fromString(ZERO_ADDRESS))) {
-        let ownerTo = Owner.load(event.params.to.toHexString());
-        if (!ownerTo) {
-            ownerTo = new Owner(event.params.to.toHexString());
-            ownerTo.createdAt = event.block.timestamp as BigInt;
-        }
-        ownerTo.treeCount = treeContract.balanceOf(event.params.to);
-        ownerTo.updatedAt = event.block.timestamp as BigInt;
-        ownerTo.save();
+    let ownerTo = Owner.load(event.params.to.toHexString());
+    if (!ownerTo) {
+        ownerTo = new Owner(event.params.to.toHexString());
+        ownerTo.createdAt = event.block.timestamp as BigInt;
     }
-    
-    if(event.params.from.notEqual(Address.fromString(ZERO_ADDRESS))) {
+    ownerTo.treeCount = ownerTo.treeCount.plus(BigInt.fromI32(1));
+
+    //@todo fix this and make it dynamic
+    if (event.params.tokenId.ge(BigInt.fromI32(10000))) {
+        ownerTo.regularTreeCount = ownerTo.regularTreeCount.plus(BigInt.fromI32(1));
+    } else {
+        ownerTo.genesisTreeCount = ownerTo.genesisTreeCount.plus(BigInt.fromI32(1));
+    }
+
+    ownerTo.updatedAt = event.block.timestamp as BigInt;
+    ownerTo.save();
+
+    if (event.params.from.notEqual(Address.fromString(ZERO_ADDRESS))) {
         let ownerFrom = Owner.load(event.params.from.toHexString());
         if (!ownerFrom) {
             ownerFrom = new Owner(event.params.from.toHexString());
             ownerFrom.createdAt = event.block.timestamp as BigInt;
         }
-        ownerFrom.treeCount = treeContract.balanceOf(event.params.from);
+
+        ownerFrom.treeCount = ownerFrom.treeCount.minus(BigInt.fromI32(1));
+
+        //@todo fix this and make it dynamic
+        if (event.params.tokenId.ge(BigInt.fromI32(10000))) {
+            ownerFrom.regularTreeCount = ownerFrom.regularTreeCount.minus(BigInt.fromI32(1));
+        } else {
+            ownerFrom.genesisTreeCount = ownerFrom.genesisTreeCount.minus(BigInt.fromI32(1));
+        }
+
         ownerFrom.updatedAt = event.block.timestamp as BigInt;
         ownerFrom.save();
     }
 
-
-    if(event.params.to.notEqual(Address.fromString(ZERO_ADDRESS))) {
-        let gb = getGlobalData();
+    let gb = getGlobalData();
+    if (event.params.to.notEqual(Address.fromString(ZERO_ADDRESS))) {
         gb.ownedTreeCount = gb.ownedTreeCount.plus(BigInt.fromI32(1));
-        gb.save();
+    } else {
+        gb.ownedTreeCount = gb.ownedTreeCount.minus(BigInt.fromI32(1));
     }
+    gb.save();
+
 }
 
 
