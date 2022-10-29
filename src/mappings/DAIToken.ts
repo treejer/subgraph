33eabe7f4bcc1c4843d2a50f26e893a1ddf10033
Amir Habibzadeh <habibzade.amir@gmail.com>
2022-10-29 20:35:13 +0300

    address field added to erc20History

--- src/mappings/DAIToken.ts
+++ src/mappings/DAIToken.ts
@@ -7,7 +7,6 @@ import { BigInt } from '@graphprotocol/graph-ts';
 import { COUNTER_ID, getCount_ERC20History, CONTRACT_DAI_TOKEN_ADDRESS } from '../helpers';
 
 export function handleTransfer(event: Transfer): void {
-    let erc20History = new ERC20History(getCount_ERC20History(COUNTER_ID).toString());
 
     let planterFrom = Planter.load(event.params.from.toHex());
     let planterTo = Planter.load(event.params.to.toHex());
@@ -15,15 +14,38 @@ export function handleTransfer(event: Transfer): void {
         return;
     }
 
-    erc20History.token = CONTRACT_DAI_TOKEN_ADDRESS;
-    erc20History.from = event.params.from.toHexString();
-    erc20History.to = event.params.to.toHexString();
-    erc20History.event = "Transfer";
-    erc20History.amount = event.params.value as BigInt;
-    erc20History.transactionHash = event.transaction.hash.toHexString();
-    erc20History.blockNumber = event.block.number;
-    erc20History.createdAt = event.block.timestamp as BigInt;
-    erc20History.save();
+    if(planterFrom) {
+
+        let erc20History = new ERC20History(getCount_ERC20History(COUNTER_ID).toString());
+        erc20History.address = event.params.from.toHexString();
+        erc20History.event = "TransferOut";
+
+        erc20History.token = CONTRACT_DAI_TOKEN_ADDRESS;
+        erc20History.from = event.params.from.toHexString();
+        erc20History.to = event.params.to.toHexString();
+        erc20History.amount = event.params.value as BigInt;
+        erc20History.transactionHash = event.transaction.hash.toHexString();
+        erc20History.blockNumber = event.block.number;
+        erc20History.createdAt = event.block.timestamp as BigInt;
+        erc20History.save();
+
+    }
+
+    if(planterTo) {
+        let erc20History = new ERC20History(getCount_ERC20History(COUNTER_ID).toString());
+        erc20History.address = event.params.to.toHexString();
+        erc20History.event = "TransferIn";
+
+        erc20History.token = CONTRACT_DAI_TOKEN_ADDRESS;
+        erc20History.from = event.params.from.toHexString();
+        erc20History.to = event.params.to.toHexString();
+        erc20History.amount = event.params.value as BigInt;
+        erc20History.transactionHash = event.transaction.hash.toHexString();
+        erc20History.blockNumber = event.block.number;
+        erc20History.createdAt = event.block.timestamp as BigInt;
+        erc20History.save();
+    } 
+    
 }
 
 
